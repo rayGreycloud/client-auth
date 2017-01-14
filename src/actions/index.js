6dbf11dc2e2d45ea5c386d0f4746c42047ef0753
rayGreycloud <rayGreycloud@gmail.com>
2017-01-14 08:43:36 -0800

    Add redux-thunk and axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,21 @@
-
+import axios from 'axios';
+// API server url
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   // Returning function instead of usual action object
-  // Access to dispatch method via thunk
+  // - Access to dispatch method via thunk
   return function(dispatch) {
     // Submit email/password to server
+    axios.post(`${ROOT_URL}/signin`, { email, password });
 
     // If good request,
-      // Update state to indicate authenticated
-      // Save JWT
-      // Redirect to route '/feature'
+    // - Update state to indicate authenticated
+    // - Save JWT
+    // - Redirect to route '/feature'
 
     // If bad request,
-      // show error to user
-      
+    // - show error to user
+
   }
 }
