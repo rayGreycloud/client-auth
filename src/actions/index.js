e0133541799ebe32c8202f51a6e46f7ff447b1f8
rayGreycloud <rayGreycloud@gmail.com>
2017-01-14 10:41:26 -0800

    Add AUTH actions and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 // API server url
 const ROOT_URL = 'http://localhost:3090';
 
@@ -7,15 +9,19 @@ export function signinUser({ email, password }) {
   // - Access to dispatch method via thunk
   return function(dispatch) {
     // Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // If good request,
-    // - Update state to indicate authenticated
-    // - Save JWT
-    // - Redirect to route '/feature'
-
-    // If bad request,
-    // - show error to user
-
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If good request,
+        // - Update state to indicate authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save JWT
+        localStorage.setItem('token', response.data.token);
+        // - Redirect to route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If bad request,
+        // - show error to user
+      });
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 // Using ES2015 syntax to rename reducer in import
 import { reducer as form } from 'redux-form';
+import authReducer from './auth_reducer';
 
-const rootReducer = combineReducers({ form });
+const rootReducer = combineReducers({
+  form,
+  auth: authReducer
+});
 
 export default rootReducer;
