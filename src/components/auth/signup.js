17287701d29d2f5639b3b82cbdb05579b56fb9f4
rayGreycloud <rayGreycloud@gmail.com>
2017-01-16 07:22:18 -0800

    Scaffold signup form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,10 +4,29 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm}} = this.props;
+
     return (
-      <div>Sign Up</div>
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input {...passwordConfirm} type="password" className="form-control" />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign Up</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm']
+})(Signup);
