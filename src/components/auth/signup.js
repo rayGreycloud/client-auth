3bff34e7e7a52bb2799d6bef45042d1ff60cffd7
rayGreycloud <rayGreycloud@gmail.com>
2017-01-16 08:31:46 -0800

    Add signup client-side validation logic

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,6 +3,7 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+
   render() {
     const { handleSubmit, fields: { email, password, passwordConfirm}} = this.props;
 
@@ -15,6 +16,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Password:</label>
           <input {...password} type="password" className="form-control" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
@@ -26,7 +28,18 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = "Passwords must match!";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
